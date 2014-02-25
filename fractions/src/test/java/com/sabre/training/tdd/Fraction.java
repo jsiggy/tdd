0b78649419f1aa579314523c60a51f8ac10335b9
John Sigler <john.sigler@gmail.com>
2014-02-25 15:06:16 -0600

    Common denominator no reduce working

--- fractions/src/test/java/com/sabre/training/tdd/Fraction.java
+++ fractions/src/test/java/com/sabre/training/tdd/Fraction.java
@@ -1,14 +1,31 @@
 package com.sabre.training.tdd;
 
 public class Fraction {
+   private int intValue;
+   private int denominator;
+
    public Fraction(int intValue) {
+      this.intValue = intValue;
+   }
+
+   public Fraction(int numerator, int denominator) {
+      intValue = numerator;
+      this.denominator = denominator;
    }
 
    public Fraction plus(Fraction that) {
-      return this;
+      return new Fraction(this.intValue + that.intValue, this.denominator);
    }
 
    int intValue() {
-      return 0;
+      return this.intValue;
+   }
+
+   public int getNumerator() {
+      return this.intValue;
+   }
+
+   public int getDenominator() {
+      return this.denominator;
    }
 }
--- fractions/src/test/java/com/sabre/training/tdd/FractionTest.java
+++ fractions/src/test/java/com/sabre/training/tdd/FractionTest.java
@@ -11,4 +11,36 @@ public class FractionTest {
       Fraction sum = new Fraction(0).plus(new Fraction(0));
       assertEquals(0, sum.intValue());
    }
+
+   @Test
+   public void nonZeroPlusZero() {
+      Fraction sum = new Fraction(5).plus(new Fraction(0));
+      assertEquals(5, sum.intValue());
+   }
+
+   @Test
+   public void zeroPlusNonZero() {
+      Fraction sum = new Fraction(5).plus(new Fraction(0));
+      assertEquals(5, sum.intValue());
+   }
+
+   @Test
+   public void nonZeroIntegerAddition() {
+      Fraction sum = new Fraction(2).plus(new Fraction(3));
+      assertEquals(5, sum.intValue());
+   }
+
+   @Test
+   public void sameDenominatorNoReduce() {
+      Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
+      assertEquals(3, sum.getNumerator());
+      assertEquals(5, sum.getDenominator());
+   }
+
+   @Test
+   public void sameDenominatorNoReduceAnotherTest() {
+      Fraction sum = new Fraction(2, 7).plus(new Fraction(3, 7));
+      assertEquals(5, sum.getNumerator());
+      assertEquals(7, sum.getDenominator());
+   }
 }
