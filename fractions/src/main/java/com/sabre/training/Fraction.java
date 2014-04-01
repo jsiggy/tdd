d8e58a5cfc02a84bd72d88cdef777c250ce9dccd
John Sigler <john.sigler@gmail.com>
2014-04-01 11:05:51 -0500

    Real fractions with numerator, denominators, etc

---
+++ fractions/src/main/java/com/sabre/training/Fraction.java
@@ -0,0 +1,46 @@
+package com.sabre.training;
+
+public class Fraction {
+   private int numerator;
+   private int denominator;
+
+   public Fraction(int numerator) {
+      this(numerator, 1);
+   }
+
+   public Fraction(int numerator, int denominator) {
+      this.numerator = numerator;
+      this.denominator = denominator;
+   }
+
+   public Fraction plus(Fraction that) {
+      if (this.denominator != that.denominator)
+         return new Fraction(this.numerator * that.denominator + that.numerator * this.denominator,
+            this.denominator * that.denominator);
+      return new Fraction(this.numerator + that.numerator, this.denominator);
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Fraction)) return false;
+
+      Fraction fraction = (Fraction) o;
+
+      if (denominator != fraction.denominator) return false;
+      if (numerator != fraction.numerator) return false;
+
+      return true;
+   }
+
+   @Override
+   public int hashCode() {
+      int result = numerator;
+      result = 31 * result + denominator;
+      return result;
+   }
+
+   @Override public String toString() {
+      return this.numerator + "/" + this.denominator;
+   }
+}
--- fractions/src/test/java/com/sabre/training/tdd/FractionTest.java
+++ fractions/src/test/java/com/sabre/training/tdd/FractionTest.java
@@ -1,5 +1,6 @@
 package com.sabre.training.tdd;
 
+import com.sabre.training.Fraction;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,38 +10,59 @@ public class FractionTest {
    @Test
    public void zeroPlusZero() {
       Fraction sum = new Fraction(0).plus(new Fraction(0));
-      assertEquals(0, sum.intValue());
+      assertEquals(new Fraction(0), sum);
    }
 
    @Test
    public void nonZeroPlusZero() {
       Fraction sum = new Fraction(5).plus(new Fraction(0));
-      assertEquals(5, sum.intValue());
+      assertEquals(new Fraction(5), sum);
    }
 
    @Test
    public void zeroPlusNonZero() {
       Fraction sum = new Fraction(5).plus(new Fraction(0));
-      assertEquals(5, sum.intValue());
+      assertEquals(new Fraction(5), sum);
    }
 
    @Test
    public void nonZeroIntegerAddition() {
       Fraction sum = new Fraction(2).plus(new Fraction(3));
-      assertEquals(5, sum.intValue());
+      assertEquals(new Fraction(5), sum);
    }
 
    @Test
    public void sameDenominatorNoReduce() {
       Fraction sum = new Fraction(1, 5).plus(new Fraction(2, 5));
-      assertEquals(3, sum.getNumerator());
-      assertEquals(5, sum.getDenominator());
+      assertEquals(new Fraction(3, 5), sum);
    }
 
    @Test
    public void sameDenominatorNoReduceAnotherTest() {
       Fraction sum = new Fraction(2, 7).plus(new Fraction(3, 7));
-      assertEquals(5, sum.getNumerator());
-      assertEquals(7, sum.getDenominator());
+      assertEquals(new Fraction(5, 7), sum);
+   }
+
+   @Test
+   public void differentDenominatorsNoReduction() {
+      Fraction plus = new Fraction(2, 3).plus(new Fraction(1, 4));
+      assertEquals(new Fraction(11, 12), plus);
+   }
+
+   @Test
+   public void compareFractionAndSameIntegerValue() {
+      assertEquals(new Fraction(3, 1), new Fraction(3));
+   }
+
+   @Test
+   public void equalsWorks() {
+      assertEquals(new Fraction(2), new Fraction(2));
+      assertEquals(new Fraction(5, 2), new Fraction(5, 2));
+   }
+
+   @Test
+   public void notEqualsWorks() {
+      assertFalse(new Fraction(2).equals(new Fraction(1)));
+      assertFalse(new Fraction(5, 2).equals(new Fraction(2, 3)));
    }
 }
